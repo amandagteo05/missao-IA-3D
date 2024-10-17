652628bef8d820ba37d2953538dc9f9393d1cd38
amandagteo05 <148063834+amandagteo05@users.noreply.github.com>
2024-10-17 08:29:57 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,8 +1,8 @@
-const nomes = ["Amanda", "Sayuri", "Pedro", "Luan", "Ashley"]
+const nomes = ["Amanda", "Sayuri", "Ashley", "luan", "Pedro", "Joao"];
 
 export function aleatorio (lista){
-  const posicao = Math.floor(Math.random()* lista.lenght);
-  return lisra[posicao];
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
 }
 
 export const nome = aleatorio(nomes)
